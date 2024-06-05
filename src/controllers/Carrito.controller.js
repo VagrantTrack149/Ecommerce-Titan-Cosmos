7bb2884841e60645b1ef5db73a3b1d31a356cc2f
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2024-06-04 21:38:39 -0600

    Añadir buscador, funcion carrito y paginas para administración

--- src/controllers/Carrito.controller.js
+++ src/controllers/Carrito.controller.js
@@ -11,3 +11,30 @@ export const getCarrito = async () => {
         throw new Error('Error al obtener el carrito: ' + error.message);
     }
 };
+
+export const updateCarritoCantidad = async (idProducto, cantidad) => {
+    try {
+        const pool = await getconnection();
+        await pool.request()
+            .input('idProducto', sql.Int, idProducto)
+            .input('Cantidad', sql.Int, cantidad)
+            .query('UPDATE carrito SET Cantidad = @Cantidad WHERE idProducto = @idProducto');
+        console.log('Cantidad actualizada correctamente.');
+    } catch (error) {
+        console.error('Error al actualizar la cantidad:', error);
+        throw new Error('Error al actualizar la cantidad: ' + error.message);
+    }
+};
+
+export const deleteCarrito = async (idProducto) => {
+    try {
+        const pool = await getconnection();
+        await pool.request()
+        .input('idProducto', sql.Int, idProducto)
+        .query('DELETE FROM carrito WHERE idProducto = @idProducto');
+        console.log('Producto Eliminado con exito');
+    } catch (error) {
+        console.error('Error al intentar eliminar el producto', error);
+        throw new Error('Error al eliminar el elemento ' + error.message);
+    }
+}
